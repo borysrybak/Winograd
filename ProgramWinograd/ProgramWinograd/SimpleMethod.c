f06f50d1655b5af019aa7106abd8781614c2c883
Borys <borys.rybak@gmail.com>
2015-01-18 20:43:20 +0100

    source update

--- ProgramWinograd/ProgramWinograd/SimpleMethod.c
+++ ProgramWinograd/ProgramWinograd/SimpleMethod.c
@@ -7,10 +7,10 @@ void ComputeMatrixSimpleMethod(void)
 	int Sum;
 	for (int i = 0; i < SizeOfMatrix; i++)
 	{
-		for (int j = 0, z = 0; j < SizeOfMatrix; j++, z += 2)
+		for (int j = 0; j < SizeOfMatrix; j++)
 		{
 			Sum = 0;
-			for (int k = 0, z = 0; k < SizeOfMatrix; k++, z += 2)
+			for (int k = 0; k < SizeOfMatrix; k++)
 			{
 				Sum = Compute('+', Sum, Compute('*', ptrptrMatrixAB[0][i][k], ptrptrMatrixAB[1][k][j]));
 			}
